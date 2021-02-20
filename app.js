fb3bf62b89ac2311b6a392ddeeb61fd807f290f8
AlbertoGruning <alberto.gruning.zen@gmail.com>
2021-02-20 19:15:48 -0300

    cap88-basicRouting

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const bodyParser = require("body-parser")
+
+const placeRoutes = require('./routes/places-routes')
+
+const app = express()
+
+app.use(placeRoutes)
+
+app.listen(5000)
+
+//console.clear()  
+console.log('listening to port 5000')
