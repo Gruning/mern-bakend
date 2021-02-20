5e555f3e8a15502bf16ed0b7c020e37e509cfce9
AlbertoGruning <alberto.gruning.zen@gmail.com>
2021-02-20 20:55:44 -0300

    cap90-getPlacesByUserId

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -16,7 +16,13 @@ const DUMMY_PLACES = [{
 
 router.get('/:pid',(req,res,next)=>{
     const placeId = req.params.pid
-    const place = DUMMY_PLACES.find(p => p.id === placeId)
+    const place = DUMMY_PLACES.find(x => x.id === placeId)
+    res.json({place})
+})
+
+router.get('/user/:uid',(req,res,next)=>{
+    const userId = req.params.uid
+    const place = DUMMY_PLACES.find(x => x.creator === userId)
     res.json({place})
 })
 
