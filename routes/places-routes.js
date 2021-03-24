bb9628c8092456bfef3dec520951dcd5ff7a3971
AlbertoGruning <alberto.gruning.zen@gmail.com>
2021-03-24 20:57:40 -0300

    cap93-addingControllers

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,40 +1,9 @@
 const express = require('express')
-
+const placesControllers = require('../controllers/places-controllers')
 const HttpError = require('../Models/http-error')
-
 const router = express.Router()
 
-const DUMMY_PLACES = [{
-    id:'p1',
-    title:'Empire State Building',
-    description:'One famous building',
-    location:{
-        lat:40.7484474,
-        lng:-73.9871516
-    },
-    address:'20 W 34 St, New York, NY10001',
-    creator:'u1'
-}]
-
-router.get('/:pid',(req,res,next)=>{
-    const placeId = req.params.pid
-    const place = DUMMY_PLACES.find(x => x.id === placeId)
-
-    if(!place){
-        throw new HttpError('Place not found',404)
-    } 
-
-    res.json({place})
-})
-
-router.get('/user/:uid',(req,res,next)=>{
-    const userId = req.params.uid
-    const place = DUMMY_PLACES.find(x => x.creator === userId)
-    if(!place){
-        //return next(new HttpError('Place for provided user not found'))
-        throw new HttpError('Place for provided user not found',404) 
-    }
-    res.json({place})
-})
+router.get('/:pid',placesControllers.getPlaceById)
+router.get('/user/:uid',placesControllers.getPlaceByUserId)
 
 module.exports = router
