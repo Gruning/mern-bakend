91fbae3bce835bc105b920b18e99ead20e85664b
AlbertoGruning <alberto.gruning.zen@gmail.com>
2021-04-17 18:44:23 -0300

    cap95-unsoportedRoute-ok

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ const express = require('express')
 const bodyParser = require("body-parser")
 
 const placeRoutes = require('./routes/places-routes')
+const HttpError = require('./Models/http-error')
 
 const app = express()
 
 app.use(bodyParser.json())
 
 app.use('/api/places',placeRoutes)
+
+app.use((req,res,next)=>{
+    const error = new HttpError('Route not found',404)
+    throw error
+})
 
 app.use((error,req,res,next)=>{
     if (res.headerSent) {
