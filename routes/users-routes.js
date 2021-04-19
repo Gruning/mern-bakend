9b5ebd43d93501d9db0d5fb0d584e6628a19fa58
AlbertoGruning <alberto.gruning.zen@gmail.com>
2021-04-19 19:50:42 -0300

    routes

---
+++ routes/users-routes.js
@@ -0,0 +1,11 @@
+
+const express = require('express')
+const usersControllers = require('../controllers/users-controllers')
+const HttpError = require('../Models/http-error')
+const router = express.Router()
+
+router.get('/',)
+router.post('/signup',)
+router.post('/login',)
+
+module.exports = router
