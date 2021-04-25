d1764a617c2be38db9ee3917f7982886bea3ba9e
AlbertoGruning <alberto.gruning.zen@gmail.com>
2021-04-24 22:37:45 -0300

    location

--- util/location.js
+++ util/location.js
@@ -1,3 +1,5 @@
+const axios= require('axios')
+const HttpError = require('../Models/http-error')
 const API_KEY ='AIzaSyDMdaDwaIBbcOr8SKXJRgf3MolOSEQAZEU'
 
 async function getCoordinatesForAddress(address){
@@ -6,5 +8,16 @@ async function getCoordinatesForAddress(address){
     //    lng:-73.9871516
     //}
 
-    
+    const response= await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${API_KEY}`)
+
+    const data = response.data
+    if(!data || data.status ==='ZERO_RESULTS'){
+        const error= new HttpError('No location for that location',422)
+        throw error
+    }
+
+    const coordinates = data.results[0].geometry.location
+    return coordinates
 }
+
+module.exports= getCoordinatesForAddress
