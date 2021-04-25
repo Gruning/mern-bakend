46d0adde39e211dc010103ef02248f69c8ea4012
AlbertoGruning <alberto.gruning.zen@gmail.com>
2021-04-24 21:49:13 -0300

    feat: :heavy_plus_sign: axios
    install axios and create locationJs
    
    cap 102

---
+++ util/location.js
@@ -0,0 +1,10 @@
+const API_KEY ='AIzaSyDMdaDwaIBbcOr8SKXJRgf3MolOSEQAZEU'
+
+async function getCoordinatesForAddress(address){
+    //return {
+    //    lat:40.7484474,
+    //    lng:-73.9871516
+    //}
+
+    
+}
