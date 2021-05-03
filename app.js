6ee3433737841df3cdf09bb138cc0b3f56576d20
AlbertoGruning <alberto.gruning.zen@gmail.com>
2021-05-02 21:00:58 -0300

    feat: :sparkles: mongo
    install mongoose and connect to mongodb
    
    cap 122 ok

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser")
 const placesRoutes = require('./routes/places-routes')
 const usersRoutes = require('./routes/users-routes')
 const HttpError = require('./Models/http-error')
+const mongoose = require('mongoose')
 
 const app = express()
 
@@ -27,5 +28,10 @@ app.use((error,req,res,next)=>{
 
 app.listen(5000)
 
-//console.clear()  
-console.log('listening to port 5000')
+//console.clear() 
+mongoose
+    .connect('mongodb+srv://gruningzen:Esfera3010@cluster0.uo1tr.mongodb.net/places?retryWrites=true&w=majority')
+    .then(()=>{
+        console.log('listening to port 5000')
+    })
+    .catch()
