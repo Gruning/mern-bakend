d0d5d80c3b0db2c9fa4cef4af9efd5a155ea0e48
AlbertoGruning <alberto.gruning.zen@gmail.com>
2021-05-15 21:52:11 -0300

    feat: :sparkles: cap131
    createUser
    
    OK

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,5 +1,6 @@
 const { v4: uuidv4 } = require('uuid')
 const HttpError = require('../Models/http-error')
+const User = require('../Models/user')
 const {validationResult}= require('express-validator')
 
 const DUMMY_USERS=[{
@@ -20,19 +21,22 @@ const signup= async (req,res,next)=>{
         return next(new HttpError('Invalid inputs',422))
     }
     const {name,email,password,places} = req.body
-   
+  
+    
     let existingUser
+    
     try{
         existingUser= await User.findOne({email:email})
     } catch(err){
         const error= new HttpError('Error searching for registered users',500)
         return next(error)
     }
-
+    
     if(existingUser){
         const error = new HttpError('User exists, please login',422)
         return next(error)
     }
+    
 
     const createdUser = new User({
         name, 
