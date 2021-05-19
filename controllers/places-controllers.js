16ebfed3a0fe57c520f7ef1232f2fd86c1e6eebb
AlbertoGruning <alberto.gruning.zen@gmail.com>
2021-05-18 21:45:32 -0300

    feat: delete
    deleting place and removing it to the user
    
    cap 136 ok

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -133,15 +133,26 @@ const deletePlace = async (req,res,next)=>{
     const placeId = req.params.pid
     let place
     try {
-      place = await Place.findById(placeId) 
+      place = await Place.findById(placeId).populate('creator') 
       console.log(place) 
     } catch (err) {
       const error = new HttpError('Error finding place',500)
       return next(error)
     }
 
+    if(!place){
+        const error= new HttpError('Place not found', 404)
+        return next(error)
+    }
+
     try {
-      await place.remove()
+      const sess= await mongoose.startSession()
+        sess.startTransaction()
+        await place.remove({session: sess})
+        place.creator.places.pull(place)
+        await place.creator.save({session: sess})
+      await sess.commitTransaction() 
+
     } catch (err) {
       const error = new HttpError('Error deleting place',500)
       return next(error)
