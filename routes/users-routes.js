4311bc723129f6c7a278ea964ee02434c5163210
AlbertoGruning-Fedora <alberto.gruning.zen@gmail.com>
2021-06-20 15:44:33 -0300

    feat: :construction: cap 163
    install package multer

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,15 +1,20 @@
 const express = require('express')
 const usersControllers = require('../controllers/users-controllers')
+const fileUpload= require('../middleware/file-upload')
 const HttpError = require('../Models/http-error')
 const router = express.Router()
 const {check}= require('express-validator')
 
 router.get('/',usersControllers.getUsers)
-router.post('/signup',[
+
+router.post(
+    '/signup',
+    fileUpload.single('image'),
+    [
     check('name').not().isEmpty(),
     check('email').normalizeEmail().isEmail(),
     check('password').isLength({min:6})
-],usersControllers.signup)
+    ],usersControllers.signup)
 router.post('/login',usersControllers.login)
 
 module.exports = router
