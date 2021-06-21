1d1794f11accf8e725043f3fd228588a7f13d219
AlbertoGruning-Fedora <alberto.gruning.zen@gmail.com>
2021-06-21 15:36:16 -0300

    cap164-fix

--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -1,29 +1,29 @@
 const multer = require('multer')
-const { v1: uuidv1 } = require('uuid')
+const { v4: uuidv4 } = require('uuid')
 //const uuid = require('uuid/v1')
 
 const MIME_TYPE_MAP = {
     'image/png': 'png',
     'image/jpeg': 'jpeg',
-    'image/jpg': 'jpg'
+    'image/jpg': 'jpg',
 }
 
 const fileUpload = multer({
-    limits:500000,
-    storage:multer.diskStorage({
-        destination:(req,file,cb)=>{
-            cb(null,'uploads/images')
+    limits: 500000,
+    storage: multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, 'uploads/images')
         },
-        filename:(req,file,cb)=>{
-           const ext = MIME_TYPE_MAP[file.mimetype] 
-           cb(null, uuidv1()+'.'+ ext )
+        filename: (req, file, cb) => {
+            const ext = MIME_TYPE_MAP[file.mimetype]
+            cb(null, uuidv4()+'.' + ext)
         },
-        fileFilter:(req,file,cb)=>{
+        fileFilter: (req, file, cb) => {
             const isValid = !!MIME_TYPE_MAP[file.mimetype]
-            let error = isValid ? null: new Error('Invalid MIME type')
-            cb(error,isValid)
-        }
-    })
+            let error = isValid ? null : new Error('Invalid MIME type')
+            cb(error, isValid)
+        },
+    }),
 })
 
 module.exports = fileUpload
