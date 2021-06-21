3aa446784c56aeffef5aabf4c4b622443840f4f6
AlbertoGruning-Fedora <alberto.gruning.zen@gmail.com>
2021-06-21 13:28:25 -0300

    feat: :sparkles: cap 164
    filter files on the bakend
    
    cap 164 ok

--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -17,6 +17,11 @@ const fileUpload = multer({
         filename:(req,file,cb)=>{
            const ext = MIME_TYPE_MAP[file.mimetype] 
            cb(null, uuidv1()+'.'+ ext )
+        },
+        fileFilter:(req,file,cb)=>{
+            const isValid = !!MIME_TYPE_MAP[file.mimetype]
+            let error = isValid ? null: new Error('Invalid MIME type')
+            cb(error,isValid)
         }
     })
 })
