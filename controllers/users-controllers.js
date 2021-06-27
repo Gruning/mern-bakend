a8cc9548bd4a7bdeac5ef7668fb1696d9e4a32b6
AlbertoGruning-Sway <alberto.gruning.zen@gmail.com>
2021-06-27 20:18:40 -0300

    feat: :sparkles: cap 176
    generate jwt on bakend
    
    cap 176 ok

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -111,9 +111,23 @@ const login= async (req,res,next)=>{
         return next(error)
     }
 
+    let token
+    try {
+    token = jwt.sign(
+        {userId:existingUser.id, email:existingUser.email},
+        'supersecret_dont_share',
+        {expiresIn:'1h'}        
+        )
+        
+    } catch (err) {
+       const error = new HttpError('Token error while logging, try again later',500)
+       return next(error)
+        
+    }
    res.json({
-       message:'Logged in!', 
-       user: existingUser.toObject({getters:true})
+    userId:existingUser.id,
+    email:existingUser.email, 
+    token:token    
     }) 
 }
 
